0c2d60e1b7b69d41079b98cbeceea0be40d4f2bf
aadityajoshi151 <aadityajoshi151@gmail.com>
2021-03-25 13:33:21 +0530

    Update gui.py
    created "selectformat()" which runs when an item is selected from dd.
    messagebox is displayed if quality format is selected.
    fixed typo.
    minor padding changes.

--- gui.py
+++ gui.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
 
 def selectfiles():
     print("Select Files Clicked!")
@@ -8,6 +10,11 @@ def reset():
     print("Reset Button Clicked")
 def rename():
     print("Rename Button Clicked")
+def selectformat(event):
+    global finalformat
+    finalformat = selectedformat.get()
+    if finalformat.find("Quality")>-1:
+        messagebox.showinfo("Title","You Have Selected Format Which Involves Quality. If Quality Is Not Present In The Title, It Will Be Omitted")
 
 root = Tk()
 selectedformat = StringVar()
@@ -24,8 +31,8 @@ formats = [
 "Movie Name [Filesize]",
 "Movie Name (Year) [Quality]",
 "Movie Name [Year] (Quality)",
-"Movie Name (Qualtiy) [Filesize]",
-"Movie Name [Qualtiy] (Filesize)",
+"Movie Name (Quality) [Filesize]",
+"Movie Name [Quality] (Filesize)",
 "Movie Name (Year) [Qualtity] {Filesize}"
 ]
 selectedformat.set(formats[0])
@@ -34,9 +41,9 @@ root.geometry("350x250")
 Label(root,text="Welcome To Movie Renamer",pady=10).pack()
 Button(root,text="Select File(s)",command=selectfiles).pack()
 Button(root,text="Select Folder(s)",command=selectfolders).pack()
-formatdd = OptionMenu(root,selectedformat,*formats)
+formatdd = OptionMenu(root,selectedformat,*formats,command=selectformat)
 formatdd.config(width=40,pady=5)
-formatdd.pack()
+formatdd.pack(pady=5)
 dualaudiocheckbox = Checkbutton(text = "Add Dual Audio Suffix (If Exists)", variable = dualaudiocheck,
                  onvalue = 1, offvalue = 0,)
 subfoldercheckbox = Checkbutton(text = "Rename Files Inside Folder As Well", variable = subfoldercheck,
