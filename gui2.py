bada1032345981acebcbce51a6eba8926f68b3c4
aadityajoshi151 <aadityajoshi151@gmail.com>
2021-03-27 17:00:26 +0530

    Update gui2.py
    -added dropdown

--- gui2.py
+++ gui2.py
@@ -18,10 +18,32 @@ def rename():
         elif os.path.isdir(root.directory+"/"+i):
             print(i+" Is A Folder")
 
+def selectformat(event):
+    finalformat = selectedformat.get()
+    if finalformat.find("Quality")>-1:
+        messagebox.showinfo("Title","You have selected format which involves 'quality'. If 'quality' is not present in the title, it will be omitted.")
+
 
 root = Tk()
 root.title("Movie Renamer")
 root.geometry("400x400")
+
+selectedformat = StringVar()
+selectedformat.set("Select A Format")
+formats = [
+"Movie Name",
+"Movie Name (Year)",
+"Movie Name [Year]",
+"Movie Name (Quality)",
+"Movie Name [Quality]",
+"Movie Name (Filesize)",
+"Movie Name [Filesize]",
+"Movie Name (Year) [Quality]",
+"Movie Name [Year] (Quality)",
+"Movie Name (Quality) [Filesize]",
+"Movie Name [Quality] (Filesize)",
+"Movie Name (Year) [Quality] {Filesize}"
+]
 pathfield = Entry(root,width=60)
 pathfield.pack(padx=10,pady=10)
 
@@ -30,6 +52,10 @@ browzebutton.pack(padx=10,pady=10)
 
 displaybox = Listbox(root,width=60,selectmode=EXTENDED)
 displaybox.pack()
+
+formatdd = OptionMenu(root,selectedformat,*formats,command=selectformat)
+formatdd.config(width=40,pady=5)
+formatdd.pack(pady=10)
 
 renamebutton = Button(root,text="Rename",padx=15,pady=15,command=rename)
 renamebutton.pack(side=RIGHT,padx=10,pady=10)
